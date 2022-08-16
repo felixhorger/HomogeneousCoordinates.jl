94465e8f27118dfe9dbf722e962c8b959da0edd3
Felix Horger <felix.horger@kcl.ac.uk>
2022-08-16 09:36:17 +0100

    Solved bug, one based indexing. Added functions to construct transformation matrices.

--- src/HomogeneousCoordinates.jl
+++ src/HomogeneousCoordinates.jl
@@ -4,32 +4,36 @@ module HomogeneousCoordinates
 	using Interpolations
 	using StaticArrays
 
-	export transform
-
 	"""
 		Not sure if this is true for all homogeneous coordinates, but this requires
 		the matrix [4,4] element to be equal to one.
 	"""
-	function transform(a::AbstractArray{<: Real, 3}, b_size::NTuple{N, Int64}, Ma::AbstractMatrix{<: Real}, Mb::AbstractMatrix{<: Real}) where N
+	function transform(
+		a::AbstractArray{T, 3},
+		b_size::NTuple{N, Int64},
+		Ma::AbstractMatrix{<: Real},
+		Mb::AbstractMatrix{<: Real},
+		interp_scheme::Interpolations.InterpolationType,
+		extrap_scheme::Union{Int, Interpolations.BoundaryCondition}
+	) where {N, T <: Number}
 		# TODO generated for any dimension
 		
 		(rows, columns, slices) = size(a)
-		
 
-		b = zeros(b_size)
+		b = zeros(T, b_size)
 		a_itp = extrapolate(
 			interpolate(
 				a,
-				BSpline(Linear())
+				interp_scheme
 			),
-			0
+			extrap_scheme
 		)
 
 		Mba = (inv(Ma) * Mb)[1:3, :]
 		for i in CartesianIndices(b_size)
-			b_vec = SVector{4, Float64}(i[1], i[2], i[3], 1)
+			b_vec = SVector{4, Float64}(i[1]-1, i[2]-1, i[3]-1, 1)
 			a_vec = Mba * b_vec
-			#a_vec = a_vec ./ a_vec[4] 
+			#a_vec = a_vec ./ a_vec[4]
 			b[i] = a_itp(a_vec...)
 		end
 		return b
@@ -52,6 +56,59 @@ module HomogeneousCoordinates
 	#		dropdims(extrema(transformed_corners; dims=2); dims=2)
 	#	)
 	#end
+
+	function R_y(α::Real)
+		sine, cosine = sincos(α)
+		return [
+			cosine	0	sine;
+			0		1	0;
+			-sine	0	cosine
+		]
+	end
+	function R_z(α::Real)
+		sine, cosine = sincos(α)
+		return [
+			cosine	-sine	0;
+			sine	cosine	0;
+			0		0		1;
+		]
+	end
+
+	"""
+		axis in spherical coordinates (ϕ, θ) points along the third axis of a volume,
+		α gives rotation of that volume around this axis
+		(first rotation around second axis by θ, then rotation around third axis by ϕ)
+	"""
+	@inline volume_rotation(ϕ::Real, θ::Real, α::Real) = begin
+	R_z(ϕ) * R_y(θ) * R_z(α)
+	end
+
+	"""
+		Columns are basis vectors
+	"""
+	function adjust_lhrh!(e::AbstractMatrix{<: Real})
+		i = argmax(abs.(e); dims=1)
+		signs = sign.(e[i])
+		e .*= signs
+		return
+	end
+
+
+	function transformation_matrix(R::AbstractMatrix{<: Real}, voxelsize::NTuple{3, Real}, translation::NTuple{3, Real})
+		# Allocate space
+		M = Matrix{Float64}(undef, 4, 4)
+		M[1:3, 1:3] = R
+		# Scale by voxelsize
+		for i = 1:3
+			M[1:3, i] .*= voxelsize[i]
+		end
+		# Add translation
+		M[1:3, 4] .= translation
+		# Fill residual elements
+		M[4, 1:3] .= 0
+		M[4, 4] = 1
+		return M
+	end
 
 end
 
