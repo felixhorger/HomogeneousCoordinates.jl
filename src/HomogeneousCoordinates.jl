dcddc1ad077c4cbaaa0de7f55e81284d2a10b41b
Felix Horger <felix.horger@kcl.ac.uk>
2022-10-05 17:38:47 +0100

    Solved bugs in transform (N-dim), addedd convention that shift is centre of volume (integer), improved transformation_matrix()

--- src/HomogeneousCoordinates.jl
+++ src/HomogeneousCoordinates.jl
@@ -7,7 +7,7 @@ module HomogeneousCoordinates
 
 	"""
 		Not sure if this is true for all homogeneous coordinates, but this requires
-		the matrix [4,4] element to be equal to one.
+		the matrix [N,N] element to be equal to one.
 	"""
 	@generated function transform(
 		a::AbstractArray{T, 3},
@@ -29,14 +29,22 @@ module HomogeneousCoordinates
 				extrap_scheme
 			)
 
+			# Get joint transformation matrix
 			Mba = (inv(Ma) * Mb)[1:$N, :]
-			# Compute offset due to one based indexing
-			shift = (@SVector ones(Float64, $N)) - Mba * (@SVector ones(Float64, $(N+1)))
+			# Compute offset due to one based indexing and origin of the array
+			let
+				b_shift = Vector{Float64}(undef, $N+1)
+				b_shift[1:3] = collect(Float64, 1 .+ b_size .÷ 2)
+				b_shift[4] = 1
+				@views shift = collect(Float64, 1 .+ size(a) .÷ 2) - Mba[1:3, :] * b_shift
+				Mba[1:3, $N+1] += shift
+			end
+
 			# Iterate indices of b
 			for i in CartesianIndices(b_size)
-				b_vec = SVector{4, Float64}(i[1], i[2], i[3], 1)
-				a_vec = Mba * b_vec + shift
-				#a_vec = a_vec ./ a_vec[4]
+				b_vec = SVector{$N+1, Float64}(Tuple(i)..., 1)
+				a_vec = Mba * b_vec
+				#a_vec = a_vec ./ a_vec[N]
 				b[i] = $(Expr(:call, :a_itp, ntuple(d -> :(a_vec[$d]), N)...))
 			end
 			return b
@@ -86,6 +94,7 @@ module HomogeneousCoordinates
 	@inline volume_rotation(ϕ::Real, θ::Real, α::Real) = R_z(ϕ) * R_y(θ) * R_z(α)
 
 	function cosines2spherical(dc::AbstractVector{<: Real})
+		@assert length(dc) == 3
 		φ = atan(dc[2], dc[1])
 		θ = atan(sqrt(dc[1]^2 + dc[2]^2), dc[3])
 		return φ, θ
@@ -103,25 +112,33 @@ module HomogeneousCoordinates
 		return
 	end
 
-	function transformation_matrix(R::AbstractMatrix{<: Real}, voxelsize::NTuple{3, Real}, translation::NTuple{3, Real})
+	"""
+		R: volume rotation matrix
+		δx: voxel size
+		Δx: translation of volume
+	"""
+	function transformation_matrix(R::AbstractMatrix{<: Real}, δx::AbstractVector{<: Real}, Δx::AbstractVector{<: Real})
+		n = size(R, 1)
+		@assert n == size(R, 2) == length(δx) == length(Δx)
 		# Allocate space
-		M = Matrix{Float64}(undef, 4, 4)
-		M[1:3, 1:3] = R
-		# Scale by voxelsize
-		for i = 1:3
-			M[1:3, i] .*= voxelsize[i]
-		end
-		# Add translation
-		M[1:3, 4] .= translation
+		M = Matrix{Float64}(undef, n+1, n+1)
+		# Rotation
+		M[1:n, 1:n] = R
+		# Scale
+		scale!(M, δx)
+		# Translate
+		M[1:n, n+1] .= Δx
 		# Fill residual elements
-		M[4, 1:3] .= 0
-		M[4, 4] = 1
+		M[n+1, 1:n] .= 0
+		M[n+1, n+1] = 1
 		return M
 	end
 
-	function scale!(M::AbstractMatrix{<: Real}, δ::AbstractVector{<: Real})
-		for i = 1:3
-			M[1:3, i] .*= δ[i]
+	function scale!(M::AbstractMatrix{<: Real}, δx::AbstractVector{<: Real})
+		n = size(M, 1)
+		@assert n == size(M, 2) == length(δx)+1
+		for i = 1:n-1
+			M[1:n-1, i] .*= δx[i]
 		end
 		return
 	end
